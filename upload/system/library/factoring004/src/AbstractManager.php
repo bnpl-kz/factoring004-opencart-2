ffc9b692218f43fdb421dc013dbb6a18441d60c5
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-04-04 15:16:31 -0700

    Adds abstract manager

---
+++ upload/system/library/factoring004/src/AbstractManager.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BnplPartners\Factoring004Payment;
+
+use BnplPartners\Factoring004\Api;
+use BnplPartners\Factoring004\Auth\BearerTokenAuth;
+use Config;
+use Log;
+use Registry;
+
+abstract class AbstractManager
+{
+    /**
+     * @var \Config
+     */
+    protected $config;
+
+    /**
+     * @var \Log
+     */
+    protected $log;
+
+    /**
+     * @var \BnplPartners\Factoring004\Api
+     */
+    protected $api;
+
+    /**
+     * @var string[]
+     */
+    protected $confirmableDeliveries;
+
+    public function __construct(Config $config, Log $log)
+    {
+        $this->config = $config;
+        $this->log = $log;
+        $this->api = Api::create(
+            $config->get('payment_factoring004_api_host'),
+            new BearerTokenAuth($config->get('payment_factoring004_delivery_token')),
+        );
+        $this->confirmableDeliveries = $this->parseConfirmableDeliveries();
+    }
+
+    /**
+     * @return static
+     */
+    public static function create(Registry $registry): AbstractManager
+    {
+        return new static($registry->get('config'), $registry->get('log'));
+    }
+
+    abstract public function getOrderStatusId(): string;
+
+    /**
+     * @return string[]
+     */
+    protected function parseConfirmableDeliveries(): array
+    {
+        $raw = $this->config->get('payment_factoring004_delivery');
+
+        return $raw ? explode(',', $raw) : [];
+    }
+
+    /**
+     * @param array<string, mixed> $order
+     */
+    protected function isOtpConfirmable(array $order): bool
+    {
+        $shippingCode = $this->parseShippingCode($order['shipping_code']);
+
+        return in_array($shippingCode, $this->confirmableDeliveries);
+    }
+
+    protected function parseShippingCode(string $shippingCode): string
+    {
+        return explode('.', $shippingCode, 2)[0];
+    }
+}

eed8a35792b8a09258beabfce5b3fe6bb8bd9999
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-05-04 13:58:06 +0600

    Adds debug logger

---
+++ upload/system/library/factoring004/src/DebugLogger.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BnplPartners\Factoring004Payment;
+
+use Log;
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+
+class DebugLogger extends AbstractLogger
+{
+    /**
+     * @var \Log
+     */
+    private $log;
+
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        if ($level !== LogLevel::DEBUG) {
+            return;
+        }
+
+        $this->log->write($message);
+    }
+}
